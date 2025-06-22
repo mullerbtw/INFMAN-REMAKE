ef71816f2b41efc9bdfc1e7ab11beddcd0a734b4
ismael müller <ismuller04@gmail.com>
2025-06-22 16:41:27 -0300

    formatted binary_file_generator

--- utility/binary_file_generator.c
+++ utility/binary_file_generator.c
@@ -4,32 +4,32 @@
 
 typedef struct
 {
-    char nomeJog[12];
-    int pontuacao;
+  char nomeJog[12];
+  int pontuacao;
 } POSICOES;
 
 int main()
 {
-    POSICOES leaderboard [5] =
-    {
-        {"MARCELO", 850},
-        {"GABRIEL", 800},
-        {"FRANCISCO", 600},
-        {"ISMAEL", 500},
-        {"ADRIANA", 200}
-    };
+  POSICOES leaderboard [5] =
+  {
+    {"MARCELO", 850},
+    {"GABRIEL", 800},
+    {"FRANCISCO", 600},
+    {"ISMAEL", 500},
+    {"ADRIANA", 200}
+  };
+  
+  FILE *fp;
+  fp = fopen("top_scores.bin", "wb");
 
-    FILE *fp;
-    fp = fopen("top_scores.bin", "wb");
+  if (fp != NULL)
+  {
+    if(fwrite(leaderboard, sizeof(POSICOES), 5, fp) != 5)
+        printf("ERRO NA LEITURA!");
+    else
+    printf("ARQUIVO LIDO COM SUCESSO!");
+  }
+  fclose(fp);
 
-    if (fp != NULL)
-    {
-        if(fwrite(leaderboard, sizeof(POSICOES), 5, fp) != 5)
-            printf("ERRO NA LEITURA!");
-        else
-        printf("ARQUIVO LIDO COM SUCESSO!");
-    }
-    fclose(fp);
-
-    return 0;
+  return 0;
 }
